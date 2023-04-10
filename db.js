d58dff4fedff890df1aa0315838b81c1ca3e9504
李祥贇 <74708246+li-xiangyun@users.noreply.github.com>
2023-04-10 19:49:30 +0800

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ let mongoose = require('mongoose')
 mongoose.connect(`mongodb://127.0.0.1/test${config.DB}`);
 
 let db = mongoose.connection;
-db.on('error',err=>{
+db.on('error',(err)=>{
     console.log(err);
 })
 db.once('open',()=>{
